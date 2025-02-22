77443d212607d0337ae07a230b6697e321a155f2
huynbr <huynh.bryan@gmail.com>
2025-02-22 11:28:13 -0500

    Ajoutation du modele Utilisateur

---
+++ src/main/java/ca/cal/tp2/modele/Utilisateur.java
@@ -0,0 +1,22 @@
+package ca.cal.tp2.modele;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "utilisateur")
+@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String email;
+    private String telephone;
+}
