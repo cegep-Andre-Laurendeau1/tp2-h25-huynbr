331dc641cba0b74e84c5f787d8d095a733cd7d50
huynbr <huynh.bryan@gmail.com>
2025-02-22 11:50:30 -0500

    Ajoutation du modele Emprunteur

---
+++ src/main/java/ca/cal/tp2/modele/Emprunteur.java
@@ -0,0 +1,22 @@
+package ca.cal.tp2.modele;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+
+@Entity
+@Table(name = "emprunteur")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Emprunteur extends Utilisateur{
+//    @OneToMany(mappedBy = "emprunteur")
+//    private Set<Emprunt> emprunts = new HashSet<>();
+}
