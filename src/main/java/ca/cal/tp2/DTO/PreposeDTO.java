c25573e7e470cdae4751ab72d547426707ddbd78
huynbr <huynh.bryan@gmail.com>
2025-02-24 14:13:57 -0500

    Ajoutation de PreposeDTO

---
+++ src/main/java/ca/cal/tp2/DTO/PreposeDTO.java
@@ -0,0 +1,21 @@
+package ca.cal.tp2.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PreposeDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String address;
+    private String email;
+    private int telephone;
+}
