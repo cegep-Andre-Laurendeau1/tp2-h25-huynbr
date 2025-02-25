7bcbea3c471534a78e4bba0b20d86778ec4e23d5
huynbr <huynh.bryan@gmail.com>
2025-02-25 18:05:22 -0500

    Implementation de DocumentDAOImpl findById et save sont complete

---
+++ src/main/java/ca/cal/tp2/DAO/DocumentDAOImpl.java
@@ -0,0 +1,52 @@
+package ca.cal.tp2.DAO;
+
+import ca.cal.tp2.modele.Document;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public class DocumentDAOImpl implements DocumentDAO {
+    private EntityManager entityManager;
+
+    public DocumentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    public Document findById(Long id) {
+        return entityManager.find(Document.class, id);
+    }
+
+    @Override
+    public void save(Document document) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(document);
+            entityManager.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            throw e;
+        }
+    }
+
+    @Override
+    public List<Document> searchBytitre(String titre) {
+        return List.of();
+    }
+
+    @Override
+    public List<Document> findByAuteur(String auteur) {
+        return List.of();
+    }
+
+    @Override
+    public List<Document> findByYear(int annee) {
+        return List.of();
+    }
+
+    @Override
+    public List<Document> findByEditeur(String editeur) {
+        return List.of();
+    }
+}
