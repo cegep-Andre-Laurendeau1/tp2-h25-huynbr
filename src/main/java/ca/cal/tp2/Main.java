438610cd79c8d0bb5bf10aebb303dd5c6d5ec0ba
huynbr <huynh.bryan@gmail.com>
2025-02-27 14:49:44 -0500

    Implementation du Plus dAjoutement dun Livre au bibliotheque

--- src/main/java/ca/cal/tp2/Main.java
+++ src/main/java/ca/cal/tp2/Main.java
@@ -32,6 +32,7 @@ public class Main {
         EmprunteurDTO emprunteur = emprunteurService.createEmprunteur("Smith", "John", "rue 1234", "johnsmith@gmail.com", "5141234567");
         System.out.println("Enregistrement d'un Emprunteur est Réussi : " + emprunteur.getId() + " " + emprunteur.getNom() + " " + emprunteur.getPrenom() + " " + emprunteur.getEmail() + " "
                 + emprunteur.getAddress()+ " " + emprunteur.getTelephone() );
+
         Prepose prepose1 = preposeService.createPrepose("Doe", "Jane", "rue 0987", "janedoe@gmail.com", "5145554321");
         System.out.println("---------------------------------------------------------------------------");
 
@@ -47,14 +48,26 @@ public class Main {
         System.out.println("---------------------------------------------------------------------------");
 
         System.out.println("Ajout d'un DVD à la bibliothèque...");
-        DVDDTO dvddto = new DVDDTO(null, "Star wars", "George Lucas", "Editeur2", 1977, 1, 120, "Sci-fi");
+        DVDDTO dvddto = new DVDDTO(null, "Akira", "Katsuhiro Otomo", "Editeur2", 1988, 1, 124, "Sci-fi");
         preposeService.addDVD(dvddto);
         System.out.println("DVD ajouté à la bibliothèque avec succès.");
         System.out.println("---------------------------------------------------------------------------");
 
         System.out.println("Ajout d'un Livre à la bibliothèque...");
-        LivreDTO livreDTO = new LivreDTO(null, "Lord of the ring", "Tolkien", "Editeur", 1954, 0, 1216, "Fantasy");
+        LivreDTO livreDTO = new LivreDTO(null, "Lord of the ring", "Tolkien", "Editeur3", 1954, 0, 1216, "Fantasy");
         preposeService.addLivre(livreDTO);
+        System.out.println("Livre ajouté à la bibliothèque avec succès.");
+        System.out.println("---------------------------------------------------------------------------");
+
+        System.out.println("Ajout d'un Livre à la bibliothèque...");
+        LivreDTO livreDTO2 = new LivreDTO(null, "Dune", "Frank Herbert", "Editeur4", 1965, 5, 900, "Sci-fi");
+        preposeService.addLivre(livreDTO2);
+        System.out.println("Livre ajouté à la bibliothèque avec succès.");
+        System.out.println("---------------------------------------------------------------------------");
+
+        System.out.println("Ajout d'un Livre à la bibliothèque...");
+        LivreDTO livreDTO3 = new LivreDTO(null, "Harry Potter", "JK Rowling", "Editeur5", 1977, 2, 309, "Fantasy");
+        preposeService.addLivre(livreDTO3);
         System.out.println("Livre ajouté à la bibliothèque avec succès.");
         System.out.println("---------------------------------------------------------------------------");
     }
